574d06cc454274e425d2c96c231af0cc56173b8e
Emanuel-GAJ <99967639+Emanuel-GAJ@users.noreply.github.com>
2023-05-09 14:48:44 -0300

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-const domain = 'http://localhost:3000';
-
-async function read(resource) {
-    const res = await fetch(`${domain}${resource}`);
-
-    return await res.json();
-}
-
-export default { read };
